7dc29783d85d37a5d60ddacae1738e9f01cf1b2f
Jeff Ferrell <jwferrell78@gmail.com>
2017-11-03 16:33:56 -0700

    Practicing with string helper methods.
    Broke down strings into indexes and reordered with various for loops, split strings into arrays and applied character padding, and cleaned up a strange string with index targeted helper methods.

---
+++ ChallengePhunWithStrings/PhunWithStrings.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhunWithStrings.aspx.cs" Inherits="ChallengePhunWithStrings.PhunWithStrings" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="reverseNameLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="reverseSequenceLabel" runat="server"></asp:Label>
+        <br />
+    
+    </div>
+        <asp:Label ID="paddedNamesLabel" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="puzzleLabel" runat="server"></asp:Label>
+        <br />
+    </form>
+</body>
+</html>
---
+++ ChallengePhunWithStrings/PhunWithStrings.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class PhunWithStrings : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // 1.  Reverse the order of the characters of the following string:
+            string name = "Bob Tabor";
+                      
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverseNameLabel.Text += name[i];
+            }
+
+
+            // 2.  Reverse this sequence:
+            string names = "Luke,Leia,Han,Chewbacca";
+            string[] nameArray = names.Split(',');
+            string result = "";
+
+            for (int i = nameArray.Length - 1; i >= 0; i--)
+            {
+                result += nameArray[i] + ", ";
+            }
+            result = result.Remove(result.Length -2, 1);
+            reverseSequenceLabel.Text = result;
+
+
+            // 3. Use the sequence to create ascii art:
+
+            string rebels = "";
+            for (int i = 0; i < nameArray.Length; i++)
+            {
+                int leftSide = (14 - nameArray[i].Length) / 2;
+                string paddingL = nameArray[i].PadLeft(nameArray[i].Length + leftSide, '*');
+                rebels += paddingL.PadRight(14, '*') + " <br/>";
+            }
+            paddedNamesLabel.Text = "<br/>" + rebels;
+
+            // End resuly should display as:
+            //*****Luke*****
+            //*****Leia*****
+            //*****Han******
+            //**Chewbacca***
+            
+
+            // 4. Solve this puzzle:
+
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+
+            string removeMe = "remove-me";
+            int cut = puzzle.IndexOf("remove-me");
+            puzzle = puzzle.Remove(cut, removeMe.Length);
+            puzzle = puzzle.ToLower().Remove(0, 1).Insert(0, "N").Replace("z","t");
+
+            puzzleLabel.Text = puzzle;
+
+            // Once you fix it with string helper methods, it should read:
+            // Now is the time for all good men to come to the aid of their country.
+        }
+    }
+}
